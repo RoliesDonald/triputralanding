cbe858489c376e5208d5332e3957993b2a98e24d
RoliesDonald <rolies.donald@gmail.com>
2020-06-20 05:05:49 +0700

    sabtu pagi

--- assets/js/landio.js
+++ assets/js/landio.js
@@ -158,6 +158,19 @@
     });
   }
 
+  // Gallery
+  $(function() {
+    var selectedClass = "";
+    $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+    $("."+selectedClass).fadeIn().addClass('animation');
+    $("#gallery").fadeTo(300, 1);
+    }, 300);
+    });
+    });
 
   function init() {
     onScrollAnimations();
