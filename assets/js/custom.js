acdba90dee4e6b5d7db6c70320742b7145ea6669
Rolies Donald <rolies.donald@gmail.com>
2020-12-23 02:33:39 +0700

    instafeed gallery magnific

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    var feed = new Instafeed({
+    get: "user",
+    userId: '30341990860',
+    limit: 6,
+    target: 'instafeed',
+    accessToken: 'IGQVJYRzJIMWw5QldnN092MFItbldYdWliYXg2RkRDV0NMemdBMkI5SWlMX2MydS1oMzJxVjJhd3lWR0lGYW5tTFlIa0pNMnlUcFdkU01XdHFBX3pKcXhia19DVUpPN3hUYjBqUEpwb1pBLU5CVko2VQZDZD',
+    sortBy: 'most-recent',
+    // template: '<div class="col-xs-12 col-sm-6 col-lg-4 instaimg"><a href="{{image}}"><div class="img-featured-container"><div class="img-backdrop"></div><div class="description-container"><p class="caption">{{caption}}</p><span class="likes"><i class="icon ion-heart"></i> {{likes}}</span><span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span></div><img src="{{image}}" class="img-responsive"></div></a></div>',
+    template: '<div class="col-lg-4 gallery instaimg"><a href="{{image}}" title="{{caption}}"><img src="{{image}}" alt="{{caption}}" class="img-fluid" /></a></div>',
+    });
+    feed.run();
+
+    $('.gallery').magnificPopup({
+  type: 'image',
+  delegate: 'a',
+  gallery:{enabled:true}
+});
+});
