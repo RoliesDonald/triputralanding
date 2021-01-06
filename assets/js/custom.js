3eb11a40114748e288a0df28430e7360fe0e8ef9
Rolies Donald <rolies.donald@gmail.com>
2021-01-07 05:54:02 +0700

    delete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,8 +8,19 @@ $(document).ready(function() {
     accessToken: 'IGQVJYRzJIMWw5QldnN092MFItbldYdWliYXg2RkRDV0NMemdBMkI5SWlMX2MydS1oMzJxVjJhd3lWR0lGYW5tTFlIa0pNMnlUcFdkU01XdHFBX3pKcXhia19DVUpPN3hUYjBqUEpwb1pBLU5CVko2VQZDZD',
     sortBy: 'most-recent',
     // template: '<div class="col-xs-12 col-sm-6 col-lg-4 instaimg"><a href="{{image}}"><div class="img-featured-container"><div class="img-backdrop"></div><div class="description-container"><p class="caption">{{caption}}</p><span class="likes"><i class="icon ion-heart"></i> {{likes}}</span><span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span></div><img src="{{image}}" class="img-responsive"></div></a></div>',
-    template: '<div class="col-lg-4 gallery instaimg"><a href="{{image}}" title="{{caption}}"><img src="{{image}}" alt="{{caption}}" class="img-fluid" /></a></div>',
-    });
+    template: '<div class="col-lg-4 gallery-title instaimg"><a href="{{image}}"><img src="{{image}}" alt="{{caption}}" class="img-fluid" /></a></div>',
+    },
+    {
+      get: "user",
+      userId: '30341990860',
+      limit: 6,
+      target: 'sadeea',
+      accessToken: 'IGQVJYRzJIMWw5QldnN092MFItbldYdWliYXg2RkRDV0NMemdBMkI5SWlMX2MydS1oMzJxVjJhd3lWR0lGYW5tTFlIa0pNMnlUcFdkU01XdHFBX3pKcXhia19DVUpPN3hUYjBqUEpwb1pBLU5CVko2VQZDZD',
+      sortBy: 'most-recent',
+      // template: '<div class="col-xs-12 col-sm-6 col-lg-4 instaimg"><a href="{{image}}"><div class="img-featured-container"><div class="img-backdrop"></div><div class="description-container"><p class="caption">{{caption}}</p><span class="likes"><i class="icon ion-heart"></i> {{likes}}</span><span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span></div><img src="{{image}}" class="img-responsive"></div></a></div>',
+      template: '<div class="col-lg-4 gallery-title instaimg"><a href="{{image}}"><img src="{{image}}" alt="{{caption}}" class="img-fluid" /></a></div>',
+      }
+    );
     feed.run();
 
     $('.gallery').magnificPopup({
